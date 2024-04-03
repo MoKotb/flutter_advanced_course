2d19677928e6127cb4441e6c5bb7094a0c2f376b
Mohamed Kotb <mohamed.kotb@rabbitmart.com>
2024-04-03 16:16:07 +0200

    no message

--- lib/features/login/logic/cubit/login_state.dart
+++ lib/features/login/logic/cubit/login_state.dart
@@ -8,7 +8,5 @@ class LoginState<T> with _$LoginState<T> {
   const factory LoginState.initial() = _Initial;
   const factory LoginState.loading() = Loading;
   const factory LoginState.success(T data) = Success<T>;
-
-  
   const factory LoginState.error({required String error}) = Error;
 }
